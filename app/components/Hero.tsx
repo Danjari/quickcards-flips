3173c916d1aeee644b4ce0b4125df141ced19780
Danjari <mm12515@nyu.edu>
2024-08-21 16:48:27 -0400

    version 0.2

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,35 +3,6 @@
 const Hero = () => {
    
     return (
-        // <header
-        //     id="home"
-        //     className="flex flex-col w-full h-screen max-w-7xl items-center justify-center p-8 relative overflow-x-hidden"
-        // >
-        //     <div className="w-full h-full flex flex-col justify-center items-center gap-8">
-        //         <div className="flex flex-col gap-2 items-center text-center">
-        //             <h1 className="text-6xl font-black md:text-8xl">
-        //                 QuickCards
-        //             </h1>
-        //             <h2 className="text-2xl">
-        //                 Never Forget Any Concept Again!
-        //             </h2>
-        //         </div>
-
- 
-
-        //         <p className='max-w-md text-sm md:text-base text-zinc-500 text-center'>QuickCards is an AI-powered Flash Card Generator that helps
-        //             you remember anything</p>
-                
-        //         <div className='w-full flex items-center justify-center'>
-        //         <a href='#pricing' className="px-6 py-3 text-sm sm:text-base rounded-full bg-white text-black hover:bg-fuchsia-700 hover:text-white transition-colors">
-        //            Try it for free!
-        //             </a>
-        //         </div>
-        //     </div>
-        // </header>
-
-        
-      
         <section id="home" className="relative isolate px-6 pt-14 lg:px-8">
         <div
           aria-hidden="true"
